0c3f870792319c815882d9aca7fad7f2ced44075
Nikhil <nikhil.kapadia.97@gmail.com>
2020-11-24 20:47:24 -0500

    Changed response display. Need to implement data calls

--- bot.js
+++ bot.js
@@ -7,16 +7,64 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if (msg.content === '!stock help') {
-        msg.reply('!stock find - Find current price of a stock\n' +
-            '!stock alert - Get Alerts about a stock');
+    if(!msg.content.startsWith(process.env.PREFIX)) return;
+
+    let data = msg.content.substring(0, msg.content.length).split(' ');
+    let cmd = data[1];
+
+    if (data[0] === '!stock') {
+        switch (cmd) {
+            case 'help':
+                msg.channel.send({
+                    embed: {
+                        color: 3447003,
+                        title: 'Stock Bot Help',
+                        description: "Commands for the bot",
+                        fields: [
+                            {
+                                name: "!stock find [ticker]",
+                                value: "Find current stock price for ticker"
+                            },
+                            {
+                                name: "!stock alert [ticker] [price]",
+                                value: "Set alert for ticker to reach certain price"
+                            }
+                        ],
+                        footer: {
+                            text: "Values in [square brackets] are required"
+                        }
+                    }
+                });
+                break;
+            case 'find':
+                msg.channel.send({
+                    embed: {
+                        color: 3447003,
+                        title: "Enter stock ticker here",
+                        fields: [
+                            {
+                                value: "Show stock data"
+                            }
+                        ]
+                    }
+                });
+                break;
+            case 'alert':
+                msg.channel.send({
+                    embed: {
+                        color: 3447003,
+                        title: "Alert",
+                        fields: [
+                            {
+                                value: "Alert has been set up for ticker - "
+                            }
+                        ]
+                    }
+                });
+                break;
+        }
     }
-    if (msg.content === '!stock find') {
-        msg.reply('Figure out how to add stock name');
-    }
-    if (msg.content === '!stock alert') {
-        msg.reply('Figure out how to add alerts & connect to api');
-    }
+
 });
 
 client.login(process.env.TOKEN);
